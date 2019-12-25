a08272f0e9dbcfc40d31019ec0a9b512af31bbb6
Alexandra Vystavkina <a.vystavkina@yandex.ru>
2019-12-25 16:13:44 +0300

    first commit

---
+++ element-search/popups/task.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let modalMain = document.getElementById('modal_main');
+modalMain.className += ' modal_active';
+
+let modalClose = document.querySelectorAll('div.modal__close');
+for(let i = 0; i < modalClose.length; i++) {
+	modalClose[i].onclick = () => moduleClose();
+}
+
+let modalSuccess = document.querySelector('#modal_success');
+let showSuccess = document.querySelector('.show-success');
+
+showSuccess.onclick = () => {
+	moduleClose();
+	modalSuccess.className += ' modal_active';
+};
+
+function moduleClose () {
+	let elementActive = document.querySelector('.modal_active');
+	elementActive.className = elementActive.className.replace(' modal_active', '');
+}
---
+++ element-search/slider/task.js
@@ -0,0 +1,46 @@
+'use strict';
+
+let sliderArrow = document.getElementsByClassName('slider__arrow');
+let sliderItem = document.querySelectorAll('.slider__item');
+let sliderDot = document.querySelectorAll('.slider__dot');
+let index = 0;
+
+for (let i = 0; i < sliderArrow.length; i++) {
+	sliderArrow[i].onclick = changeSlide;
+}
+
+for (let i = 0; i < sliderDot.length; i++) {
+	sliderDot[i].onclick = () => {dotSlide(i)};
+}
+
+function changeSlide (key) {
+	let classSlideArrow = event.target.classList;
+	
+	sliderItem[index].classList.remove('slider__item_active');
+
+
+	if(classSlideArrow.contains('slider__arrow_next')) {
+		if (index == sliderItem.length - 1) {
+			index = 0;
+		} else {
+			index++;
+		}
+	}
+
+	if(classSlideArrow.contains('slider__arrow_prev')) {
+
+		if (index == 0) {
+			index = sliderItem.length - 1;
+		} else {
+			index--;
+		}
+	}
+
+	sliderItem[index].classList.add('slider__item_active');
+}
+
+function dotSlide(key) {
+	sliderItem[index].classList.remove('slider__item_active');
+	sliderItem[i].classList.add('slider__item_active')
+	index = key;
+}
